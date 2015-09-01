1b68d3b9f690d621ebbd9d10dd0c63bf0f0564ed
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2015-09-01 10:29:03 +0200

    initial commi

---
+++ light.js
@@ -0,0 +1,26 @@
+var raspi = require('raspi-io');
+var five = require('johnny-five');
+var Firebase = require('firebase');
+var lightRef = new Firebase('https://lettherebelight.firebaseio.com/switch');
+var board = new five.Board({
+  io: new raspi()
+});
+
+board.on('ready', function() {
+        var lamp = new five.Relay({pin:'P1-5', type: 'NC'});
+        var tv = new five.Relay({pin:'P1-22', type: 'NC'});
+        this.repl.inject({
+                lamp: lamp,
+                tv: tv
+        });
+        lightRef.on('value', function(data){
+                if(data.val() == "true"){
+                        console.log("turning on "+data.val());
+                        tv.on();
+                }
+                else{
+                        console.log("turning off " +data.val());
+                        tv.off();
+                }
+        });
+});
