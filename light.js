f1c21b702150c5f59fae1c7f987536c5645a10a9
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2015-09-01 14:49:40 +0200

    variable fixes

--- light.js
+++ light.js
@@ -7,20 +7,19 @@ var board = new five.Board({
 });
 
 board.on('ready', function() {
-        var lamp = new five.Relay({pin:'P1-5', type: 'NC'});
-        var tv = new five.Relay({pin:'P1-22', type: 'NC'});
+        var lamp = new five.Relay({pin:'P1-22', type: 'NC'});
+        var tv = new five.Relay({pin:'P1-21', type: 'NC'});
         this.repl.inject({
-                lamp: lamp,
-                tv: tv
+            lamp: lamp,
+            tv: tv
         });
         lightRef.on('value', function(data){
-                if(data.val() == "true"){
-                        console.log("turning on "+data.val());
-                        tv.on();
-                }
-                else{
-                        console.log("turning off " +data.val());
-                        tv.off();
-                }
+            if(data.val() == "true"){
+                    console.log("turning on "+data.val());
+                    lamp.on();
+            } else{
+                    console.log("turning off " +data.val());
+                    lamp.off();
+            }
         });
 });
